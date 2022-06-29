57ece664f7752fec3b0898d3b52c61a786f663b0
komiljon4717 <komiljon4717@gmail.com>
2022-06-29 16:34:17 +0500

    Api key qo'shildi

--- src/config.js
+++ src/config.js
@@ -1,12 +1,16 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+
 const { env } = process
+process.pictures = []
+process.organs = []
 
 export default {
     TOKEN: env.TOKEN,
     POSTGRES: env.POSTGRES,
     emailaddress: env.MYEMAIL,
-    pass: env.EMAIL_PASS
+    pass: env.EMAIL_PASS,
+    API_KEY: env.API_KEY,
     
 }
